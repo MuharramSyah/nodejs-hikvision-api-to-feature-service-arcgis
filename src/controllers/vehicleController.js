0028bd6da3fd894d0770c22ad9e7057c695fc438
muharramsyah <msyah@esriindonesia.co.id>
2023-04-02 07:13:44 +0700

    Initial Commit

---
+++ src/controllers/vehicleController.js
@@ -0,0 +1,18 @@
+module.exports = {
+    /**
+     * Information
+     * - License plat number
+     * - Vehicle Color
+     * - Person Name
+     * - Phone Number
+     * - Vehicle Id
+     * - vehicleGroupIndexCode
+     * @param {*} req 
+     * @param {*} res 
+     */
+    information: (req, res) => {
+        var data = req.param('data');
+
+        /* TODO */
+    },
+}
---
+++ src/utils/eventType.js
@@ -0,0 +1,22 @@
+const eventType = {
+    'motion': 131331,
+    'parking_alarm': 131591,
+    'people_gathering': 131593,
+    'fast_moving_alarm': 131592,
+    'violent_motion': 131596,
+    'climbing': 131597,
+    'falling_down': 131605,
+    'stick_up': 131601,
+    'people_density': 131672,
+    'physical_conflict': 131678,
+    'wrong_way_driving': 131679,
+    'people_queue': 133122,
+    'fire_detection': 192515,
+    'reverse_entering_alarm': 200497,
+    'illegal_lane_change': 225309,
+    'illegal_parking': 254101,
+    'PIR': 364545,
+    'hard_hat': 1417219,
+}
+
+module.exports = eventType
